e5ea6c195aea8f1b127414a8c67aed093b86da99
Wojciech Babinski <wbabinski@gmail.com>
2015-07-17 07:10:54 +0100

    django-registration-redux implemented

--- growthstreet/settings.py
+++ growthstreet/settings.py
@@ -37,7 +37,17 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registration',
 )
+
+# registration settings
+REGISTRATION_OPEN = True  # If True, users can register
+ACCOUNT_ACTIVATION_DAYS = 7  # One-week activation window; you may, of course, use a different value.
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+# and are trying to access pages requiring authentication
+# end registration settings
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -55,7 +65,7 @@ ROOT_URLCONF = 'growthstreet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- growthstreet/urls.py
+++ growthstreet/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-]
+from .views import IndexPageView
+
+urlpatterns = patterns('',
+                       url(r'^$', IndexPageView.as_view(), name="index"),
+                       url(r'^admin/', include(admin.site.urls)),
+                       (r'^accounts/', include('registration.backends.simple.urls')),
+                       )
